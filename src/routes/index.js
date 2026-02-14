e9f725d098a2ced6f5ebd7d3ae701c858eaf3451
nguyentaituananh <anhnttph48578@gmail.com>
2026-02-14 19:57:48 +0700

    update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
 import Router from "express";
 import authRouter from "../modules/auth/auth.routes.js";
 import tablesRouter from "../modules/tables/tables.routes.js";
+import categoriesRouter from "../modules/categories/categories.routes.js";
+import productsRouter from "../modules/products/products.routes.js";
+import dishesRouter from "../modules/dishes/dishes.routes.js";
 
 const router = Router();
 
-router.use("/auth",authRouter)
-router.use("/tables",tablesRouter)
+router.use("/auth", authRouter);
+router.use("/tables", tablesRouter);
+router.use("/categories", categoriesRouter);
+router.use("/products", productsRouter);
+router.use("/dishes", dishesRouter);
 
 export default router;
