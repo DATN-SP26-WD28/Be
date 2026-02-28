7ecf69ce1e5ff7bd9e2e59fa0bad4af156ceba7f
nguyentaituananh <anhnttph48578@gmail.com>
2026-02-28 23:28:03 +0700

    delete product

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ import Router from "express";
 import authRouter from "../modules/auth/auth.routes.js";
 import tablesRouter from "../modules/tables/tables.routes.js";
 import categoriesRouter from "../modules/categories/categories.routes.js";
-import productsRouter from "../modules/products/products.routes.js";
 import dishesRouter from "../modules/dishes/dishes.routes.js";
 
 const router = Router();
@@ -10,7 +9,6 @@ const router = Router();
 router.use("/auth", authRouter);
 router.use("/tables", tablesRouter);
 router.use("/categories", categoriesRouter);
-router.use("/products", productsRouter);
 router.use("/dishes", dishesRouter);
 
 export default router;
