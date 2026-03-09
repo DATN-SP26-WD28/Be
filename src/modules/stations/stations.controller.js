eda61fe1d95a88695768fe945ab28bcf896ea697
Tuấn Khanh <tuankhanh111904@gmail.com>
2026-03-09 09:13:55 +0700

    create station

--- src/modules/stations/stations.controller.js
+++ src/modules/stations/stations.controller.js
@@ -1,3 +1,4 @@
+import createError from '../../shared/utils/createError.js';
 import createResponse from '../../shared/utils/createResponse.js';
 import handleAsync from '../../shared/utils/handleAsync.js';
 import Station from './stations.model.js';
@@ -7,5 +8,18 @@ import Station from './stations.model.js';
 export const getAllStations = handleAsync(async (req, res) => {
   const stations = await Station.find().sort({ createdAt: -1 });
   return createResponse(res, 200, 'Lấy danh sách trạm thành công', stations);
+});
+
+// 2. Tạo trạm chế biến mới (Dành cho Admin)
+export const createStation = handleAsync(async (req, res) => {
+  const { name, printer_ip, description } = req.body;
+
+  const existingStation = await Station.findOne({ name });
+  if (existingStation) {
+    throw createError(res, 400, 'Tên trạm này đã tồn tại');
+  }
+
+  const newStation = await Station.create({ name, printer_ip, description });
+  return createResponse(res, 201, 'Thêm trạm mới thành công', newStation);
 });
 
--- src/modules/stations/stations.routes.js
+++ src/modules/stations/stations.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllStations } from './stations.controller.js';
+import { createStation, getAllStations } from './stations.controller.js';
 
 const stationsRouter = express.Router();
 
 stationsRouter.get('/', getAllStations);
+stationsRouter.post('/', createStation);
 
 
 export default stationsRouter;
