c89bc937c955e6a5ac116a4efcab87c96efe4a6f
nguyentaituananh <anhnttph48578@gmail.com>
2026-04-15 00:08:19 +0700

    restore dishes

--- src/modules/dishes/dishes.model.js
+++ src/modules/dishes/dishes.model.js
@@ -12,13 +12,15 @@ const dishSchema = new mongoose.Schema(
     price: {
       type: Number,
       required: true,
-      min: [0, 'Giá tiền không thể nhỏ hơn 0']
-    }, status: {
+      min: [0, "Giá tiền không thể nhỏ hơn 0"],
+    },
+    status: {
       type: String,
-      enum: ['available', 'out_of_stock'],
-      default: 'available',
+      enum: ["available", "out_of_stock"],
+      default: "available",
     },
     image_url: { type: String, maxLength: 255 },
+    deleted_at: { type: Date, default: null },
   },
   { timestamps: true },
 );
--- src/modules/dishes/dishes.routes.js
+++ src/modules/dishes/dishes.routes.js
@@ -2,17 +2,21 @@ import express from "express";
 import {
   createDish,
   getDishes,
+  getDeletedDishes,
   getDishById,
   updateDish,
   deleteDish,
+  restoreDish,
 } from "./dishes.controller.js";
 
 const dishesRouter = express.Router();
 
-dishesRouter.post("/", createDish);
 dishesRouter.get("/", getDishes);
+dishesRouter.post("/", createDish);
+dishesRouter.get("/deleted", getDeletedDishes);
 dishesRouter.get("/:id", getDishById);
 dishesRouter.put("/:id", updateDish);
 dishesRouter.delete("/:id", deleteDish);
+dishesRouter.patch("/:id/restore", restoreDish);
 
 export default dishesRouter;
